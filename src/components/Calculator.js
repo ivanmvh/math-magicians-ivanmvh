8613121d61c401feee6c47dcac5fad7edcdd0446
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-11 05:46:39 -0500

    Giving some style

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,27 +9,27 @@ class Calculator extends React.Component {
   render() {
     return (
       <section className="calculator-container">
-        <h3 className="result-screen">0</h3>
+        <h3 className="display-screen">0</h3>
         <div className="calculator-buttons">
           <button type="button" className="btncalc">AC</button>
           <button type="button" className="btncalc">+/-</button>
           <button type="button" className="btncalc">%</button>
-          <button type="button" className="btncalc color-right">÷</button>
+          <button type="button" className="btncalc oper-button">÷</button>
           <button type="button" className="btncalc">7</button>
           <button type="button" className="btncalc">8</button>
           <button type="button" className="btncalc">9</button>
-          <button type="button" className="btncalc color-right">x</button>
+          <button type="button" className="btncalc oper-button">x</button>
           <button type="button" className="btncalc">4</button>
           <button type="button" className="btncalc">5</button>
           <button type="button" className="btncalc">6</button>
-          <button type="button" className="btncalc color-right">-</button>
+          <button type="button" className="btncalc oper-button">-</button>
           <button type="button" className="btncalc">1</button>
           <button type="button" className="btncalc">2</button>
           <button type="button" className="btncalc">3</button>
-          <button type="button" className="btncalc color-right">+</button>
-          <button type="button" className="btncalc zero">0</button>
+          <button type="button" className="btncalc oper-button">+</button>
+          <button type="button" className="btncalc zero-btn">0</button>
           <button type="button" className="btncalc">.</button>
-          <button type="button" className="btncalc color-right">=</button>
+          <button type="button" className="btncalc oper-button">=</button>
         </div>
       </section>
     );
