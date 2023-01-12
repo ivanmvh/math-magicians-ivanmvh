fcaa6e83fa9129ecc566f392defdacb8adf081d7
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-12 05:02:53 -0500

    Add handleClick method and use it in onClick event

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,6 +5,11 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
@@ -12,25 +17,25 @@ class Calculator extends React.Component {
       <section className="calculator-container">
         <h3 className="calculator-display">0</h3>
         <div className="calculator-area-buttons">
-          <button type="button" className="btncalc">AC</button>
-          <button type="button" className="btncalc">+/-</button>
-          <button type="button" className="btncalc">%</button>
-          <button type="button" className="btncalc oper-button">÷</button>
-          <button type="button" className="btncalc">7</button>
-          <button type="button" className="btncalc">8</button>
-          <button type="button" className="btncalc">9</button>
-          <button type="button" className="btncalc oper-button">x</button>
-          <button type="button" className="btncalc">4</button>
-          <button type="button" className="btncalc">5</button>
-          <button type="button" className="btncalc">6</button>
-          <button type="button" className="btncalc oper-button">-</button>
-          <button type="button" className="btncalc">1</button>
-          <button type="button" className="btncalc">2</button>
-          <button type="button" className="btncalc">3</button>
-          <button type="button" className="btncalc oper-button">+</button>
-          <button type="button" className="btncalc zero-btn">0</button>
-          <button type="button" className="btncalc">.</button>
-          <button type="button" className="btncalc oper-button">=</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">AC</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">+/-</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">%</button>
+          <button onClick={this.handleClick} type="button" className="btncalc oper-button">÷</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">7</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">8</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">9</button>
+          <button onClick={this.handleClick} type="button" className="btncalc oper-button">x</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">4</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">5</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">6</button>
+          <button onClick={this.handleClick} type="button" className="btncalc oper-button">-</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">1</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">2</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">3</button>
+          <button onClick={this.handleClick} type="button" className="btncalc oper-button">+</button>
+          <button onClick={this.handleClick} type="button" className="btncalc zero-btn">0</button>
+          <button onClick={this.handleClick} type="button" className="btncalc">.</button>
+          <button onClick={this.handleClick} type="button" className="btncalc oper-button">=</button>
         </div>
       </section>
     );
