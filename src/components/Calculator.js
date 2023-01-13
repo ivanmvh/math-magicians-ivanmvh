4bf8288283ed65f6a4cd98e688dd68cc32bcfed7
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-13 00:50:31 -0500

    Dont use this. in functions - prettier forced to use doublke quotes in jsx

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,68 +15,68 @@ const Calculator = () => {
   const { total, next, operation } = state;
 
   return (
-    <section className='calculator-container'>
-      <div className='calculator-display'>
+    <section className="calculator-container">
+      <div className="calculator-display">
         {total}
         {operation}
         {next}
       </div>
-      <div className='calculator-area-buttons'>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+      <div className="calculator-area-buttons">
+        <button onClick={handleClick} type="button" className="btncalc">
           AC
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           +/-
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           %
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+        <button onClick={handleClick} type="button" className="btncalc oper-button">
           ÷
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           7
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           8
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           9
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+        <button onClick={handleClick} type="button" className="btncalc oper-button">
           x
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           4
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           5
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           6
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+        <button onClick={handleClick} type="button" className="btncalc oper-button">
           -
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           1
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           2
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           3
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+        <button onClick={handleClick} type="button" className="btncalc oper-button">
           +
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc zero-btn'>
+        <button onClick={handleClick} type="button" className="btncalc zero-btn">
           0
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc'>
+        <button onClick={handleClick} type="button" className="btncalc">
           .
         </button>
-        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+        <button onClick={handleClick} type="button" className="btncalc oper-button">
           =
         </button>
       </div>
