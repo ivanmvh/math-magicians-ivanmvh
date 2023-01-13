a1c771c902f4559714acbc03b4df98a4f8127476
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-13 00:33:47 -0500

    Elimination of the use of render only return is used with functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,39 +12,76 @@ const Calculator = () => {
     setState((state) => calculate(state, e.target.textContent));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <section className="calculator-container">
-        <div className="calculator-display">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="calculator-area-buttons">
-          <button onClick={this.handleClick} type="button" className="btncalc">AC</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">+/-</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">%</button>
-          <button onClick={this.handleClick} type="button" className="btncalc oper-button">÷</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">7</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">8</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">9</button>
-          <button onClick={this.handleClick} type="button" className="btncalc oper-button">x</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">4</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">5</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">6</button>
-          <button onClick={this.handleClick} type="button" className="btncalc oper-button">-</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">1</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">2</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">3</button>
-          <button onClick={this.handleClick} type="button" className="btncalc oper-button">+</button>
-          <button onClick={this.handleClick} type="button" className="btncalc zero-btn">0</button>
-          <button onClick={this.handleClick} type="button" className="btncalc">.</button>
-          <button onClick={this.handleClick} type="button" className="btncalc oper-button">=</button>
-        </div>
-      </section>
-    );
-  }
-}
+  const { total, next, operation } = state;
+
+  return (
+    <section className='calculator-container'>
+      <div className='calculator-display'>
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className='calculator-area-buttons'>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          AC
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          +/-
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          %
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+          ÷
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          7
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          8
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          9
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+          x
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          4
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          5
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          6
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+          -
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          1
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          2
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          3
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+          +
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc zero-btn'>
+          0
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc'>
+          .
+        </button>
+        <button onClick={this.handleClick} type='button' className='btncalc oper-button'>
+          =
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default Calculator;
